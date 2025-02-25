08f662caa831efea8f6fa50a3e4f29c17d74b320
Emilio Villalba <emiliovianconi@gmail.com>
2025-02-25 08:40:31 -0300

    my first commit/basic page it's ok

---
+++ members/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.main, name='main'),
+    path('members/', views.members, name='members'),
+    path('members/details/<int:id>', views.details, name='details'),
+    path('testing/', views.testing, name='testing'),
+]
